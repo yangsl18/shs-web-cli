0cb684aa8e1f4f92c78fa22232ad27a2201136bf
yangshiliang <shiliang1.yang@tcl.com>
2022-06-17 10:27:12 +0800

    增加注释

--- src/main.js
+++ src/main.js
@@ -6,6 +6,9 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import locale from 'element-ui/lib/locale/lang/zh-CN' // lang i18n
 
+/**
+ * UI框架选择
+ */
 Vue.use(ElementUI, { locale })
 
 createApp(App).mount('#app')
